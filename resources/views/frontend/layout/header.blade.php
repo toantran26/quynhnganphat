8f693a48d26bfcc81698d0480e333a7983af4903
toantran26 <toantran26106@gmail.com>
2023-10-25 01:48:24 +0800

    toan tran update code header

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -11,13 +11,13 @@
             </li>
             <li class="item">
               <a href="mailto:{{ConfigSite::getInfoByName('contact_email')}}">
-                <img src="{{asset('frontend/svg/mail-icon.svg')}}" alt="">
+                <img src="{{asset('frontend/svg-v2/icon-eath.svg')}}" alt="">
                 <span>Giới thiệu</span>
               </a>
             </li>
             <li class="item">
               <a href="javascript:void(0)">
-                <img src="{{asset('frontend/svg/time-icon.svg')}}" alt="">
+                <img src="{{asset('frontend/svg-v2/icon-news.svg')}}" alt="">
                 <span>
                   Tin tức sự kiện
                 </span>
@@ -25,7 +25,7 @@
             </li>
             <li class="item">
               <a href="javascript:void(0)">
-                <img src="{{asset('frontend/svg/time-icon.svg')}}" alt="">
+                <img src="{{asset('frontend/svg-v2/icon-clsp.svg')}}" alt="">
                 <span>
                   Chất lượng sản phẩm
                 </span>
@@ -37,6 +37,10 @@
           <ul class="d-flex">
             <li class="item">
               <form action="" method="GET">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="27" viewBox="0 0 26 27" fill="none">
+                  <path d="M24.2736 22.0804L21.7382 24.6263C21.3914 24.2892 21.0376 23.9513 20.69 23.6064C19.2103 22.138 17.7332 20.6673 16.2587 19.1942C16.0682 19.0035 15.9281 19.019 15.7129 19.1498C12.9491 20.83 10.0158 21.164 6.96024 20.1519C4.85994 19.4581 3.17846 18.1556 1.89336 16.3368C0.641591 14.5853 -0.0215376 12.4776 0.000533614 10.3208C0.00790958 8.2137 0.655401 6.15924 1.85626 4.43263C3.05711 2.70603 4.75397 1.38975 6.71947 0.660164C9.08763 -0.208729 11.4899 -0.231308 13.8487 0.660164C16.6465 1.71981 18.6485 3.65691 19.8012 6.44811C20.6613 8.53003 20.7844 10.6727 20.2657 12.8535C19.98 14.0685 19.4623 15.2161 18.7414 16.2325C18.6415 16.375 18.6942 16.4513 18.7964 16.5517C19.9901 17.7313 21.1862 18.9077 22.3699 20.0974C23.014 20.7436 23.6333 21.4116 24.2736 22.0804ZM10.3031 3.01147C6.22556 3.10023 3.14982 6.48704 3.12195 10.3223C3.09331 14.3476 6.45084 17.8403 10.6452 17.7188C14.5462 17.6044 17.7954 14.3258 17.742 10.2445C17.6893 6.22622 14.3628 2.97721 10.3031 3.01147Z" fill="#919191"/>
+                  <path d="M22.4558 25.3271L25.0485 22.7578C25.3024 23.0155 25.5904 23.2725 25.8335 23.5652C26.0417 23.8151 26.0278 24.1406 25.9372 24.431C25.6275 25.4143 24.9788 26.0504 23.9701 26.2855C23.829 26.3238 23.6803 26.3229 23.5397 26.2828C23.3992 26.2428 23.2721 26.1652 23.1719 26.0582C22.9428 25.8215 22.7121 25.5879 22.4558 25.3271Z" fill="#919191"/>
+                </svg>
                 <input placeholder="Tìm kiếm">
               </form>
             </li>            
@@ -48,15 +52,10 @@
   <div class="menu__header__box">
     <div class="container-fix">
       <div class="row">
-        <div class="col-2 logo__header">
-          <a href="/">
-            <img src="{{asset('frontend/images/logo.svg')}}" alt="">
-          </a>
-        </div>
         <div class="col-8 menu__box">
           <div class="container">
             <div class="row">
-              <div class="col-11 menu__header">
+              <div class="col-12 menu__header">
                 <ul>
                   @foreach (ConfigSite::listMenu() as $key => $value)
                   <li class="item {{route('detail-cate', $value->slug) == URL::current()?'active':''}}">
@@ -81,24 +80,12 @@
                   @endforeach
                 </ul>
               </div>
-              <div class="col-1 search__icon__header">
-                <form action="{{route('search')}}" class="search__form">
-                  <input type="text" name="search" placeholder="Tìm kiếm...">
-                  <a href="javascript:void(0);" class="close-search">
-                    <img src="{{asset('frontend/svg/x.svg')}}" alt="">
-                  </a>
-                </form>
-                <a href="javascript:void(0);" class="btn-search">
-                  <img src="{{asset('frontend/svg/search-icon.svg')}}" alt="">
-                </a>
-              </div>
             </div>
           </div>
         </div>
-        <div class="col-2 chat__menu__header">
-          <a href=".">
-            <img src="{{asset('frontend/svg/chat-icon.svg')}}" alt="">
-            <span>{{__('Chat với chúng tôi')}}</span>
+        <div class="col-4 chat__menu__header">
+          <a href="#">
+            <span>Miễn phí tư vấn + Thiết kế</span>
           </a>
         </div>
       </div>
